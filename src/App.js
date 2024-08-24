a39ed5c7de680ef37a8d64d3c83388716f0d11d1
Suman Kumar <sumancsnit@gmail.com>
2024-08-24 17:52:58 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+};
+
+export default App;
